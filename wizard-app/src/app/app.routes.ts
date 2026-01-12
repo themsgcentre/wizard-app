d69d8b79c4a96eb08651737c887ea99a0da008f8
hannah <97944856+themsgcentre@users.noreply.github.com>
2026-01-12 10:57:26 +0100

    added stuff to home component

--- wizard-app/src/app/app.routes.ts
+++ wizard-app/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    component: HomeComponent
   },
   {
     path: '',
